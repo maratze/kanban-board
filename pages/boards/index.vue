590c057726380d64ce1836e536888ae1df6d411e
Ramazanov Marat <maratoff20@gmail.com>
2023-12-25 19:06:55 +0300

    Updated boards page

--- pages/boards/index.vue
+++ pages/boards/index.vue
@@ -1,34 +1,41 @@
 <template>
-  <div class="grid gap-8">
-    <div v-if="recentlyBoards.length">
-      <BoardTitle
-        iconName="streamline:fastforward-clock"
-        text="Recently" />
-      <BoardCardWrapper>
-        <BoardCard
-          v-for="board of recentlyBoards"
-          v-bind:key="board.id"
-          :board="board"
-          class="text-white" />
-      </BoardCardWrapper>
+  <div>
+    <div class="grid gap-8" v-if="hasData">
+      <div v-if="recentlyBoards.length">
+        <BoardTitle
+          iconName="streamline:fastforward-clock"
+          text="Recently" />
+        <BoardCardWrapper>
+          <BoardCard
+            v-for="board of recentlyBoards"
+            v-bind:key="board.id"
+            :board="board"
+            class="text-white" />
+        </BoardCardWrapper>
+      </div>
+      <div v-if="favoritesBoards.length">
+        <BoardTitle
+          iconName="streamline:interface-favorite-star-reward-rating-rate-social-star-media-favorite-like-stars"
+          text="Favorites" />
+        <BoardCardWrapper>
+          <BoardCard
+            v-for="board of favoritesBoards"
+            v-bind:key="board.id"
+            :board="board"
+            class="text-white" />
+        </BoardCardWrapper>
+      </div>
     </div>
-    <div v-if="favoritesBoards.length">
-      <BoardTitle
-        iconName="streamline:interface-favorite-star-reward-rating-rate-social-star-media-favorite-like-stars"
-        text="Favorites" />
-      <BoardCardWrapper>
-        <BoardCard
-          v-for="board of favoritesBoards"
-          v-bind:key="board.id"
-          :board="board"
-          class="text-white" />
-      </BoardCardWrapper>
+    <div v-else>
+      <Space
+        image-path="/image-1.svg"
+        description="To create a board please create a new workspace or add a board to existing workspace.">
+      </Space>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { Timestamp } from 'firebase/firestore';
 import type { Board } from '~/types';
 
 definePageMeta({
@@ -50,6 +57,9 @@ const recentlyBoards = computed((): Board[] => {
 })
 const favoritesBoards = computed((): Board[] => {
   return boards.value.filter(x => x.favoriteUserIds.includes(user.value.uid))
+})
+const hasData = computed((): boolean => {
+  return recentlyBoards.value.length > 0 || favoritesBoards.value.length > 0
 })
 
 loadWorkspaces().then(() => {
